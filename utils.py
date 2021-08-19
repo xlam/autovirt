6e8236ab3439ac9e4b8372e39cf507d12fc72fa2
Sergey Sokolov <xlamserg@gmail.com>
2021-08-19 22:39:11 +0400

    feat: persistent sessions
        Make session persistent to not log in every time scripts are
        executed.

--- utils.py
+++ utils.py
@@ -1,10 +1,33 @@
 # %%
 import os
 import json
+import pickle
 import logging
 import requests
+import datetime
 
 import config
+
+
+class VirtSession(requests.Session):
+
+    def __init__(self):
+        self.logger = get_logger('utils_session')
+        requests.Session.__init__(self)
+
+    def get(self, *args, **kwargs):
+        res = requests.Session.get(self, *args, **kwargs)
+        self.save_session()
+        return res
+
+    def post(self, *args, **kwargs):
+        res = requests.Session.post(self, *args, **kwargs)
+        self.save_session()
+        return res
+
+    def save_session(self):
+        with open(config.session_file, 'wb') as f:
+            pickle.dump(self, f)
 
 
 # %%
@@ -39,13 +62,37 @@ def get_logger(name: str):
 
 # %%
 def get_logged_session():
-    s = requests.Session()
-    s.post('https://virtonomica.ru/vera/main/user/login', {
-        'userData[login]': config.login,
-        'userData[password]': config.password,
-        'remember': 1,
-    })
+    logger = get_logger('utils_session')
+    s = get_cached_session()
+    if not s:
+        s = VirtSession()
+        s.post('https://virtonomica.ru/vera/main/user/login', {
+            'userData[login]': config.login,
+            'userData[password]': config.password,
+            'remember': 1,
+        })
+        logger.info('New session initialized')
     return s
+
+
+# %%
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
+
+# %%
+def get_cached_session():
+    logger = get_logger('utils_session')
+    if not os.path.exists(config.session_file):
+        return False
+    time = modification_date(config.session_file)
+    last_mod_time = (datetime.datetime.now() - time).seconds
+    if last_mod_time < config.session_timeout:
+        with open(config.session_file, 'rb') as f:
+            s = pickle.load(f)
+            logger.info(f'Cached session loaded')
+            return s
 
 
 # %%
