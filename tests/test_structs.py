39677cd6b08e9f1e2f9f3516e478456aa45b6e5f
Sergey Sokolov <xlamserg@gmail.com>
2021-11-18 21:37:19 +0400

    feat: UnitEquipment calculates expected quality
        Add expected_quality() into UnitEquipment that calculates the final
        quality after repairing weared items with given quality.

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -2,6 +2,19 @@ import pytest
 from autovirt.structs import UnitEquipment
 
 
+@pytest.fixture()
+def ue():
+    return UnitEquipment(
+        1,  # id
+        100,  # quantity
+        100,  # quantity max
+        31,  # quality installed
+        30,  # quality required
+        0,  # equipment wear, %
+        1234,  # equipment id
+    )
+
+
 @pytest.mark.parametrize(
     "quality, expected", [(35.124, 12), (35.126, 13), (35.125, 12)]
 )
@@ -18,3 +31,11 @@ def test_unit_equipment_floats_rounded(quality: float, expected: int):
     quality_string = str(ue.qual)
     decimal = int(quality_string.split(".")[1])
     assert decimal == expected
+
+
+@pytest.mark.parametrize("wear, quality, expected", [(0, 40, 31), (5, 40, 31.45)])
+def test_unit_equipment_calculates_expected_quality(
+    ue, wear: float, quality: float, expected: float
+):
+    ue.wear = wear
+    assert ue.expected_quality(quality) == expected
